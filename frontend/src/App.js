4d148ad12c9a2b1066bf8b60c6a2f73420467318
Goran Medakovic <gomedako@gmail.com>
2023-11-20 20:02:03 +0100

    prettier and lint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,7 @@ import Header from "./components/Header";
 import Search from "./components/Search";
 import { useState } from "react";
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState("");
@@ -12,27 +11,26 @@ const App = () => {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`,
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-
       })
-      
+
       .catch((err) => {
-      console.log(err);
+        console.log(err);
+      });
+    setWord("");
+  };
 
-      })
-      setWord("");
-  }
-
-  
   return (
     <div>
-      <Header title="Images Gallery"/>
+      <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
