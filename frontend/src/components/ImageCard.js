0208a8910b6c30f2cf390b830a957b1d7ef551bf
Goran Medakovic <gomedako@gmail.com>
2023-12-03 19:03:43 +0100

    saving images in the frontend app

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
 
-const ImageCard = ({ image, deleteImage }) => {
+const ImageCard = ({ image, deleteImage, saveImage }) => {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={image.urls.small} />
@@ -9,9 +9,13 @@ const ImageCard = ({ image, deleteImage }) => {
         <Card.Title>{image.title?.toUpperCase()}</Card.Title>
         <Card.Text>{image.description || image.alt_description}</Card.Text>
         <Button variant="primary" onClick={() => deleteImage(image.id)}>
-          {" "}
           Delete
-        </Button>
+        </Button>{" "}
+        {!image.saved && (
+          <Button variant="secondary" onClick={() => saveImage(image.id)}>
+            Save Image
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
